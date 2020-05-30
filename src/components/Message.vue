c568873890a587abea53d245d1d35950c1731569
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2020-05-30 14:46:30 -0500

    actualizando mensaje de gracias formulario

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -5,7 +5,7 @@
             Thank for your interest in The Pact, Powered by Epsilon.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta provident 
             error laudantium iusto facere enim pariatur repellat architecto hic quas. 
-            Veniam atque minima necessitatibus obcaecati suscipit optio dignissimos 
+            Veniam necessitatibus obcaecati suscipit optio dignissimos 
             cupiditate soluta!
         </p>
     </div>
