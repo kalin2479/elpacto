d0d8247e6aa862ac8086a44c76bbc1bea856037a
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2020-05-30 15:18:48 -0500

    actualizando mensaje de agradecimiento

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -4,7 +4,7 @@
         <p>
             Thank for your interest in The Pact, Powered by Epsilon.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta provident 
-            error laudantium iusto facere enim pariatur repellat architecto hic quas. 
+            error facere enim pariatur repellat architecto hic quas. 
             Veniam atque minima necessitatibus obcaecati suscipit optio dignissimos 
             cupiditate soluta!
         </p>
