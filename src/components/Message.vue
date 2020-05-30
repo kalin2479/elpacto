cb0a48ce0646d3ad6b8cfd9febec54704b4212ec
kalin2479 <31285015+kalin2479@users.noreply.github.com>
2020-05-30 15:21:53 -0500

    cambiando texto otra vez

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -4,7 +4,7 @@
         <p>
             Thank for your interest in The Pact, Powered by Epsilon.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta provident 
-            error facere enim pariatur repellat architecto hic quas. 
+            error esta linea esta siendo modificado desde el mismo git ( NUNCA HACER ESTO ). 
             Veniam atque minima necessitatibus obcaecati suscipit optio dignissimos 
             cupiditate soluta!
         </p>
